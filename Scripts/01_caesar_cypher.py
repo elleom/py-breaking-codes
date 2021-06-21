aef96576f7382309ed4f045204f8d71ce700c5a8
elleom <el_leo_m@hotmail.com>
2021-06-22 01:12:05 +0200

    add argparser

--- Scripts/01_caesar_cypher.py
+++ Scripts/01_caesar_cypher.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 
 # Caesar Cypher
+import textwrap
 
 import paperclip
+import argparse
 
 SYMBOLS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890 !?.'
 
@@ -22,4 +24,13 @@ def get_user_input():
 
 
 if __name__ == '__main__':
-    get_user_input()
+    parser = argparse.ArgumentParser(description='CAESAR CYPHER TOOL',
+                                     # Help Message Formatter
+                                     # which retains any formatting in description
+                                     formatter_class=argparse.RawTextHelpFormatter,
+                                     epilog=textwrap.dedent('''Example;
+                                     01_caesar_cypher.py -e encrypt [message] '''))
+    parser.add_argument('-e', '--encrypt', help='Encrypts the given message')
+    parser.add_argument('-d', '--decrypt', help='Decrypts the given message')
+
+    #todo finish
