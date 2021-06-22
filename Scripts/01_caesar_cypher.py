9c209df6be076c935efc1489bfda2805be79d511
elleom <el_leo_m@hotmail.com>
2021-06-22 11:34:33 +0200

    upd argparser

--- Scripts/01_caesar_cypher.py
+++ Scripts/01_caesar_cypher.py
@@ -9,8 +9,15 @@ import argparse
 SYMBOLS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890 !?.'
 
 
-def decrypt_message(message):
+def decrypt_message(message, key, mode):
     decrypted_message = ''
+    for symbol in message:
+        # Note: only symbos within SYMBOLS const can be traced
+        if symbol in SYMBOLS:
+            symbol_index = SYMBOLS.find(symbol)
+
+            # performs encrypt/decrypt
+            if mode == 'encrypt'
 
 
 def get_user_input():
@@ -24,13 +31,15 @@ def get_user_input():
 
 
 if __name__ == '__main__':
+    mode = ''
     parser = argparse.ArgumentParser(description='CAESAR CYPHER TOOL',
                                      # Help Message Formatter
                                      # which retains any formatting in description
                                      formatter_class=argparse.RawTextHelpFormatter,
                                      epilog=textwrap.dedent('''Example;
-                                     01_caesar_cypher.py -e encrypt [message] '''))
-    parser.add_argument('-e', '--encrypt', help='Encrypts the given message')
-    parser.add_argument('-d', '--decrypt', help='Decrypts the given message')
+                                     01_caesar_cypher.py -e encrypt [message] \ndefault = decrypt'''))
+    parser.add_argument('-e', '--encrypt', action='store_true',  help='Encrypts the given message')
+    parser.add_argument('-k', '--key', help='Transmuting key use to encrypt/decrypt')
+    args = parser.parse_args()
 
     #todo finish
