2252a45ee2a942d90b9ac65e5b7a6a4fba414108
elleom <el_leo_m@hotmail.com>
2021-06-22 12:32:26 +0200

    turned OOP

--- Scripts/01_caesar_cypher.py
+++ Scripts/01_caesar_cypher.py
@@ -2,22 +2,31 @@
 
 # Caesar Cypher
 import textwrap
-
 import paperclip
 import argparse
 
-SYMBOLS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890 !?.'
 
+class MessageProcessor:
+    SYMBOLS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890 !?.'
 
-def decrypt_message(message, key, mode):
-    decrypted_message = ''
-    for symbol in message:
-        # Note: only symbos within SYMBOLS const can be traced
-        if symbol in SYMBOLS:
-            symbol_index = SYMBOLS.find(symbol)
+    def __init__(self, args):
+        self.args = args
 
-            # performs encrypt/decrypt
-            if mode == 'encrypt'
+    def process_message(self):
+        decrypted_message = ''
+        for symbol in self.args.message:
+            # Note: only symbols within SYMBOLS const can be traced
+            if symbol in self.SYMBOLS:
+                symbol_index = self.SYMBOLS.find(symbol)
+
+                # performs encrypt/decrypt
+
+    def run(self):
+        if not self.args.key:
+            self.args.key = int(input('[*] Key not specified, please provide key>'))
+        elif not self.args.message:
+            self.args.message = int(input('[*] Message not specified, please provide message>'))
+        self.process_message()
 
 
 def get_user_input():
@@ -31,7 +40,6 @@ def get_user_input():
 
 
 if __name__ == '__main__':
-    mode = ''
     parser = argparse.ArgumentParser(description='CAESAR CYPHER TOOL',
                                      # Help Message Formatter
                                      # which retains any formatting in description
@@ -40,6 +48,9 @@ if __name__ == '__main__':
                                      01_caesar_cypher.py -e encrypt [message] \ndefault = decrypt'''))
     parser.add_argument('-e', '--encrypt', action='store_true',  help='Encrypts the given message')
     parser.add_argument('-k', '--key', help='Transmuting key use to encrypt/decrypt')
-    args = parser.parse_args()
+    parser.add_argument('-m', '--message', help='Message to be encrypted/decrypted' )
+    user_input = parser.parse_args()
+    processor = MessageProcessor(user_input)
+    processor.run()
 
-    #todo finish
+
