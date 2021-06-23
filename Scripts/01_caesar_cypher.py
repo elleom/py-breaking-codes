271a5fa0e64c2231425584e23edc7678dd4cc8d4
elleom <el_leo_m@hotmail.com>
2021-06-23 23:26:50 +0200

    wrap up

--- Scripts/01_caesar_cypher.py
+++ Scripts/01_caesar_cypher.py
@@ -43,32 +43,34 @@ class MessageProcessor:
         self.print_message(processed_message)
 
     def get_user_input(self):
-        if not self.args.key:
-            self.args.key = int(input('[*] Key not specified, please provide key>'))
-        # transforms string args to int for processing
-        self.args.key = int(self.args.key)
-        if not self.args.message:
-            self.args.message = input('[*] Message not specified, please provide message>')
+        try:
+            if not self.args.key:
+                self.args.key = int(input('[*] Key not specified, please provide key>'))
+            # transforms string args to int for processing
+            self.args.key = int(self.args.key)
+            if not self.args.message:
+                self.args.message = input('[*] Message not specified, please provide message>')
+        except KeyboardInterrupt:
+            print('[-] Program quited\n')
 
     def print_message(self, message):
         print(message)
 
 
 if __name__ == '__main__':
-    try:
-        parser = argparse.ArgumentParser(description='CAESAR CYPHER TOOL',
-                                         # Help Message Formatter
-                                         # which retains any formatting in description
-                                         formatter_class=argparse.RawTextHelpFormatter,
-                                         epilog=textwrap.dedent('''Example;
-                                         01_caesar_cypher.py -e encrypt [message] => default = decrypt'''))
-        parser.add_argument('-e', '--encrypt', action='store_true',  help='Encrypts the given message')
-        parser.add_argument('-k', '--key', help='Transmuting key use to encrypt/decrypt')
-        parser.add_argument('-m', '--message', help='Message to be encrypted/decrypted => ex: " this is a message" ' )
-        user_input = parser.parse_args()
-        processor = MessageProcessor(user_input)
-        processor.run()
-    except KeyboardInterrupt:
-        print('[-] Program quited\n')
+    parser = argparse.ArgumentParser(description='CAESAR CYPHER TOOL',
+                                     # Help Message Formatter
+                                     # which retains any formatting in description
+                                     formatter_class=argparse.RawTextHelpFormatter,
+                                     epilog=textwrap.dedent('''Example;
+                                                 01_caesar_cypher.py -e encrypt [message] => default = decrypt'''))
+    parser.add_argument('-e', '--encrypt', action='store_true', help='Encrypts the given message')
+    parser.add_argument('-k', '--key', help='Transmuting key use to encrypt/decrypt')
+    parser.add_argument('-m', '--message', help='Message to be encrypted/decrypted => ex: " this is a message" ')
+    user_input = parser.parse_args()
+    processor = MessageProcessor(user_input)
+    processor.run()
+
+
 
 
