67d452be63f5f5237ab0e1dfc22d2f7080fd8e65
elleom <el_leo_m@hotmail.com>
2021-06-25 16:14:45 +0100

    delete module init

---
+++ Scripts/encryption-decryption/02_transposition_cypher.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+# Transposition Cypher steps
+
+# Count the number of characters in the message and the key.
+# Draw a row of a number of boxes equal to the key (for example, 8 boxes
+# for a key of 8).
+# Start filling in the boxes from left to right, entering one character
+# per box.
+# When you run out of boxes but still have more characters, add another
+# row of boxes.
+# When you reach the last character, shade in the unused boxes in the
+# last row.
+# Starting from the top left and going down each column, write out the
+# characters. When you get to the bottom of a column, move to the next
+# column to the right. Skip any shaded boxes. This will be the ciphertext.
