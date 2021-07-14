7d3bc98767ca8630f24de33e6af8ec78f9adda54
elleom <el_leo_m@hotmail.com>
2021-07-14 15:13:56 +0200

    created process_method

--- Scripts/encryption-decryption/transpositionFileCypher.py
+++ Scripts/encryption-decryption/transpositionFileCypher.py
@@ -10,26 +10,57 @@ from transpositionCypher import encrypt_message as encrypt
 
 
 def main():
+
     arguments = parse_args()
+
+    print('[*] Starting process', end='', flush=True)
+    time.sleep(1)
+    for symbol in '...':
+        print(symbol, end='', flush=True)
+        time.sleep(1)
+    print('\n[*] Parsing arguments', end='', flush=True)
+    for symbol in '...':
+        print(symbol, end='', flush=True)
+        time.sleep(1)
+    print('\n[*] Adding some more drama to the script', end='', flush=True)
+    for symbol in '...':
+        print(symbol, end='', flush=True)
+        time.sleep(1)
+
     run(arguments)
 
 
 def run(arguments):
-    if not arguments.key:
-        print('Key not specified, please provide. --help for usage guide')
+    output_file_path = ''
+
+    if not arguments.file:
+        print('[!] Input File not provided')
+        print('[!] Closing... run --help for instructions')
         sys.exit()
+
+    # create output file
+    input_file_path = arguments.file
+    if not arguments.output and arguments.encrypt:
+        output_file_path = arguments.file[:-3] + 'encrypted.txt'
+    elif not arguments.output and not arguments.encrypt:
+        output_file_path = arguments.file[:-3] + 'decrypted.txt'
+    else:
+        output_file_path = arguments.output
+
+def process_file(input_file, output_file, do_encrypt, key):
+
 
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Transposition Cypher encrypt/decrypt - FILES',
                                      formatter_class=argparse.RawTextHelpFormatter,
                                      epilog=textwrap.dedent("""Example:
-        python transpositionCypherFile [--encrypt] --filepath "/path/to/file" --output-file "/path/to/file"  --key 8
+        python transpositionCypherFile [--encrypt] --file "/path/to/file" --output-file "/path/to/file"  --key 8
         NOTE => default mode is decrypt"""))
     parser.add_argument('-f', '--file', help='Text file to be encrypted/decrypted')
-    parser.add_argument('-e, --encrypt', action='store_true', help='Default mode is decrypt')
+    parser.add_argument('-e', '--encrypt', action='store_true', help='Default mode is decrypt')
     parser.add_argument('-k', '--key', help='Message Key for encrypt/decrypt')
-    parser.add_argument('-o', '--output-file', help='Output file path or filename')
+    parser.add_argument('-o', '--output', help='Output file path or filename')
     return parser.parse_args()
 
 
