db8d35c9bab61922b5536552f45a571be5229466
elleom <el_leo_m@hotmail.com>
2021-07-25 23:26:15 +0200

    created loadfile + const

--- Scripts/encryption-decryption/detectEnglish.py
+++ Scripts/encryption-decryption/detectEnglish.py
@@ -5,3 +5,21 @@
 # detectEnglish.isEnglish(someString) # Returns True or False
 # (There must be a "dictionary_file.txt" file in this directory with all
 # English words in it, one word per line.
+
+
+UPPER_LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+LETTERS_AND_SPACE = UPPER_LETTERS + UPPER_LETTERS.lower() + ' \t\n'
+
+
+def load_dictionary():
+    dictionary_file = open('../assets/words.txt')
+    dictionary_words = {}  # creates a dictionary to store the key value pairs
+    for word in dictionary_file.read().split('\n'):
+        dictionary_words[word] = None
+    dictionary_file.close()
+    return dictionary_words
+
+
+DICTIONARY_WORDS = load_dictionary()
+
+
