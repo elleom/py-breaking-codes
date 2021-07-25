5c54ba327f6ae707a1e2615c27e67c30088a831a
elleom <el_leo_m@hotmail.com>
2021-07-25 23:14:17 +0200

    added dic + file skeleton

---
+++ Scripts/encryption-decryption/detectEnglish.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+# To use, type this code:
+# import detectEnglish
+# detectEnglish.isEnglish(someString) # Returns True or False
+# (There must be a "dictionary_file.txt" file in this directory with all
+# English words in it, one word per line.
